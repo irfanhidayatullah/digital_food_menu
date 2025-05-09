f807d99046f850a8ac96b8b30b5f0bd83f676c4e
irfanhidayatullah <irfanhidayatullah.jobs@gmail.com>
2025-05-09 21:44:30 +0700

    chore: transactions Item Resource

--- app/Filament/Resources/TransactionItemsResource.php
+++ app/Filament/Resources/TransactionItemsResource.php
@@ -10,7 +10,9 @@ use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Illuminate\Contracts\Support\Htmlable;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class TransactionItemsResource extends Resource
@@ -18,6 +20,28 @@ class TransactionItemsResource extends Resource
     protected static ?string $model = TransactionItems::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function shouldRegisterNavigation(): bool
+    {
+        return false;
+    }
+
+    public static string $parentResource = TransactionResource::class;
+
+    public static function getRecordTitle(?Model $record): string|null|Htmlable
+    {
+        return $record->title;
+    }
+
+    public static function canEdit(Model $record): bool
+    {
+        return false;
+    }
+
+    public static function canCreate(): bool
+    {
+        return false;
+    }
 
     public static function form(Form $form): Form
     {
@@ -46,21 +70,6 @@ class TransactionItemsResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('transaction_id')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('foods_id')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('quantity')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('price')
-                    ->money()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('subtotal')
-                    ->numeric()
-                    ->sortable(),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
@@ -69,18 +78,28 @@ class TransactionItemsResource extends Resource
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('transaction_id')
+                    ->numeric()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('food.name')
+                    ->label('Food Name')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('quantity')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('price')
+                    ->money('IDR')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('subtotal')
+                    ->numeric()
+                    ->money('IDR')
+                    ->sortable(),
             ])
-            ->filters([
-                //
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ]);
+            ->filters([])
+            ->actions([])
+            ->bulkActions([]);
     }
 
     public static function getRelations(): array
@@ -94,8 +113,6 @@ class TransactionItemsResource extends Resource
     {
         return [
             'index' => Pages\ListTransactionItems::route('/'),
-            'create' => Pages\CreateTransactionItems::route('/create'),
-            'edit' => Pages\EditTransactionItems::route('/{record}/edit'),
         ];
     }
 }
